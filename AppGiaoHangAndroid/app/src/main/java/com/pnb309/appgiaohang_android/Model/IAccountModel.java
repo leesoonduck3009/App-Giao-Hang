ecfaed3fc3f876ab2caf7e68fca70823745102e1
PNB309 <nguyen.phucbinh445@gmail.com>
2023-11-05 18:31:53 +0700

    Update application

--- AppGiaoHangAndroid/app/src/main/java/com/pnb309/appgiaohang_android/Model/IAccountModel.java
+++
@@ -1,8 +0,0 @@
-package com.pnb309.appgiaohang_android.Model;
-
-public interface IAccountModel {
-    void checkLogin(String username, String password, OnLoginFinishedListener listener);
-    interface OnLoginFinishedListener{
-        void OnLoginListener(String token, Exception e);
-    }
-}
--- AppGiaoHangAndroid/app/src/main/java/com/pnb309/appgiaohang_android/Ultilities/GlobalVariable.java
+++
@@ -1,8 +0,0 @@
-package com.pnb309.appgiaohang_android.Ultilities;
-
-import com.pnb309.appgiaohang_android.Entity.Employee;
-
-public class GlobalVariable {
-    public static Employee CurrentEmployee;
-
-}
